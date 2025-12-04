cda718af57fc06719897d0b80f824859cdcc3b68
Supporterino <lars@roth-kl.de>
2025-12-04 19:57:43 +0100

    feat(debug): show app data directory in debug route

--- src/routes/debug.tsx
+++ src/routes/debug.tsx
@@ -1,23 +1,29 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { Button, Code } from '@mantine/core';
 import { useState } from 'react';
-import { BaseDirectory } from '@tauri-apps/api/path';
+import { BaseDirectory, appDataDir } from '@tauri-apps/api/path';
 import { readDir, readTextFile } from '@tauri-apps/plugin-fs';
 import { FlexCol } from '@/components/Layout/FlexCol.tsx';
 
 const RouteComponent = () => {
   const [debugText, setDebugText] = useState<string>('');
+  const [debugDir, setDebugDir] = useState<string>('');
 
   const list = async () => {
+    const appDataDirPath = await appDataDir();
+    setDebugDir(appDataDirPath);
     setDebugText(JSON.stringify(await readDir('.', { baseDir: BaseDirectory.AppData }), null, 2));
   };
 
   const read = async () => {
+    const appDataDirPath = await appDataDir();
+    setDebugDir(appDataDirPath);
     setDebugText(JSON.stringify(JSON.parse(await readTextFile('games.json', { baseDir: BaseDirectory.AppData })), null, 2));
   };
 
   return (
-    <FlexCol fullWidth>
+    <FlexCol fullWidth p={"md"}>
+      <Code m={"sm"} style={{ textWrap: 'balance'}}>{debugDir}</Code>
       <Code block>{debugText}</Code>
       <Button onClick={list}>List Files</Button>
       <Button onClick={read}>Read state file</Button>
